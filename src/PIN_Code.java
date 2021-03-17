c46f7aad8af2771cc0f9455900d64e21d1df66e2
MasterX01 <aakashsaxena14@gmail.com>
2021-03-17 11:41:27 +0530

    [Akash] ADD. added working UC-1 code

--- src/PIN_Code.java
+++ src/PIN_Code.java
@@ -4,5 +4,13 @@ import java.util.regex.Pattern;
 public class PIN_Code {
     public static void main(String[] args) {
         System.out.println("Welcome To PIN Code Program");
+        Scanner scan = new Scanner(System.in);
+        String exp = "^[0-9]{6,6}$";
+        System.out.print("Enter the PIN code: ");
+        String pin = scan.next();
+        if(Pattern.matches(exp, pin))
+            System.out.println("PIN Code you entered is Valid");
+        else
+            System.out.println("PIN Code you entered is Invalid");
     }
 }
